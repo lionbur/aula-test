32d585fe981063723c61d28d158262a4de558560
Lion Burger <lionbur@gmail.com>
2019-02-25 18:21:18 +0200

    feat: flow types

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 
+// $FlowFixMe
 import 'dotenv/config'
 
 function main() {
   const { S3_BUCKET } = process.env
 
+  // $FlowFixMe
   console.log(`BUCKET is ${S3_BUCKET}`)
 }
 
